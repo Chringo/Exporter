66c82ee72e8c4d55bb866bde76ef1c2fa641474b
David Wigelius <david.wigelius@gmail.com>
2016-11-02 17:08:28 +0100

    made first mll file

--- ExportBBF/ExportBBF/Source.cpp
+++ ExportBBF/ExportBBF/Source.cpp
@@ -1,3 +1,34 @@
 #include "maya_includes.h"
 
 using namespace std;
+
+EXPORT MStatus initializePlugin(MObject obj)
+{
+	// most functions will use this variable to indicate for errors
+	MStatus res = MS::kSuccess;
+
+	MFnPlugin myPlugin(obj, "Maya plugin", "1.0", "Any", &res);
+	if (MFAIL(res)) {
+		CHECK_MSTATUS(res);
+	}
+
+	MGlobal::displayInfo("Maya plugin loaded!");
+	// if res == kSuccess then the plugin has been loaded,
+	// otherwise is has not.
+	return res;
+}
+
+
+EXPORT MStatus uninitializePlugin(MObject obj)
+{
+	// simply initialize the Function set with the MObject that represents
+	// our plugin
+	MFnPlugin plugin(obj);
+
+	// if any resources have been allocated, release and free here before
+	// returning...
+
+	MGlobal::displayInfo("Maya plugin unloaded!");
+
+	return MS::kSuccess;
+}

4fc70dacb79caba8a255d984c16a7e3cef27c225
David Wigelius <david.wigelius@gmail.com>
2016-11-21 17:31:59 +0100

    UPDATE: connected a function to a button
    Managed to connect the export button to a normal function.

--- ExportBBF/ExportBBF/Source.cpp
+++ ExportBBF/ExportBBF/Source.cpp
@@ -37,30 +37,73 @@ fstream outFile("pillar.BBF", std::fstream::out | std::fstream::binary);
 //{
 //	QObject::connect(parent, SIGNAL(released()), this, SLOT(test()));
 //}
-class SignalWrapper : public QMainWindow
+//class SignalWrapper : public QMainWindow
+//{
+//	//Q_OBJECT;
+//public:
+//	explicit SignalWrapper(QWidget *parent, QPushButton* button);
+//	
+//	public slots:
+//	void handleButton();
+//private:
+//	QPushButton *m_button;
+//};
+//SignalWrapper::SignalWrapper(QWidget *parent, QPushButton* button)
+//	: QMainWindow(parent)
+//{
+//	// Create the button, make "this" the parent
+//	m_button = button;
+//	// set size and location of the button
+//	//m_button->setGeometry(QRect(QPoint(100, 100),
+//	//	QSize(200, 50)));
+//
+//	// Connect button signal to appropriate slot
+//	//connect(button, SIGNAL(clicked()), this, SLOT(handleButton()));
+//}
+//void SignalWrapper::handleButton()
+//{
+//	MGlobal::displayInfo("hejsan");
+//}
+class leif : public QObject
 {
-	Q_OBJECT
-public:
-	explicit SignalWrapper(QWidget *parent = 0);
-	SignalWrapper::SignalWrapper(QWidget *parent)
-		: QMainWindow(parent)
-	{
-		// Create the button, make "this" the parent
-		m_button = new QPushButton("My Button", this);
-		// set size and location of the button
-		m_button->setGeometry(QRect(QPoint(100, 100),
-			QSize(200, 50)));
+	//Q_OBJECT
 
-		// Connect button signal to appropriate slot
-		connect(m_button, SIGNAL(released()), this, SLOT(handleButton()));
-	}
-	private slots:
-	void handleButton();
-private:
-	QPushButton *m_button;
+	public slots:
+	void allan();
+
 };
+class SignalWrapper : public QObject
+{
+	//Q_OBJECT
+public:
+	SignalWrapper(QPushButton * sender, QWidget * parent = 0);
+			
 
+		public slots:
+	void test();
 
+};
+void SignalWrapper::test()
+{
+	MGlobal::displayInfo("hejsan");
+}
+SignalWrapper::SignalWrapper(QPushButton * sender, QWidget * parent)
+	: QObject(parent)
+{
+	QObject::connect(sender, SIGNAL(clicked()), this, SLOT(test()));
+}
+const char* gemig()
+{
+	MGlobal::displayInfo("kuk");
+	const char *hej = nullptr;
+	return hej;
+}
+void exportClicked()
+{
+	MGlobal::displayInfo("kuk");
+}
+
+QPushButton* cb;
 
 EXPORT MStatus initializePlugin(MObject obj)
 {
@@ -68,16 +111,23 @@ EXPORT MStatus initializePlugin(MObject obj)
 	MGlobal::executeCommand("string $dialog = `loadUI - uiFile ""mainwindow.ui""`");
 	MGlobal::executeCommand("showWindow $dialog");
 	QWidget * control = MQtUtil::findControl("exportButton");
-	//QCheckBox* cb = (QCheckBox*)control;
-	QPushButton* cb = (QPushButton*)control;
-	
+	QWidget * box = MQtUtil::findControl("skelBox");
+	QCheckBox* sb = (QCheckBox*)box;
+	cb = (QPushButton*)control;
 
 	QWidget * ex = cb->topLevelWidget();
 	QString helll = ex->whatsThis();
 	
-	SignalWrapper tw;
+	MGlobal::displayInfo("hejsan nu är det nu");
+	QObject::connect(cb, &QPushButton::clicked, [] {exportClicked(); });
+	//sb->setChecked(true);
 
-	//QObject::connect(cb, SIGNAL(released()), tw.parent(), SLOT(test()));
+	//SignalWrapper tw(cb);
+	//QtPrivate::FunctionPointer<
+	//QObject::connect(cb, (const void*)cb->clicked(), gemig());
+	
+	
+	
 	//cb->connect(cb, SIGNAL(cb->released()), cb->topLevelWidget(), SLOT(exportClicked()));
 	//cb->clicked.connect(SLOT(exportClicked()));
 	//cb->isChecked();
@@ -141,6 +191,9 @@ EXPORT MStatus uninitializePlugin(MObject obj)
 	// simply initialize the Function set with the MObject that represents
 	// our plugin
 	MFnPlugin plugin(obj);
+
+	//QObject::disconnect(cb, [] {exportClicked(); });
+
 
 	outFile.close();
 	// if any resources have been allocated, release and free here before
